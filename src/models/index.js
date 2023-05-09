9ca4e0d243ff7c0d4cda2c0ffc7a2c0a537d73c5
rrhoades10 <rrhoades10@yahoo.com>
2023-05-09 09:01:40 -0500

    initial commit

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Question: require("./question.model.js"),
+    Quiz: require("./quiz.model"),
+    Submission: require("./submission.model"),
+    User: require("./user.model")
+}
